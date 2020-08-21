580c7de854172d1bf40ec75411d17ae1d63e1b00
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-21 16:07:17 -0300

    refactor: move colors to util.js

--- src/help.js
+++ src/help.js
@@ -1,18 +1,19 @@
 const { stripIndent } = require("common-tags")
 const chalk = require("chalk")
+const { blue, orangeString, blueString } = require("./util")
 
 module.exports = function() {
     this.options.unshift({ flags: this._helpFlags, description: this._helpDescription })
     const help = stripIndent(chalk`
-        {rgb(255,69,0) reddit-migrate} {bgRgb(255,69,0)  }
+        {${orangeString} reddit-migrate} {${bgOrangeString}  }
 
         {bold Usage:}
-            reddit-migrate {rgb(113,147,255) [options]}
+            reddit-migrate {${blueString} [options]}
 
         {bold Options:}
             ${
                 this.options.map(option =>
-                    option.flags.padEnd(32) + chalk.rgb(113, 147, 255)(option.description)
+                    option.flags.padEnd(32) + blue(option.description)
                 ).join("\n" + " ".repeat(12))
             }
         {reset  }
---
+++ src/util.js
@@ -0,0 +1,10 @@
+const chalk = require("chalk")
+
+module.exports = {
+    orange: chalk.rgb(255, 69, 0),
+    orangeString: "rgb(255,69,0)",
+    bgOrange: chalk.bgRgb(255, 69, 0),
+    bgOrangeString: "bgRgb(255,69,0)",
+    blue: chalk.rgb(113, 147, 255),
+    blueString: "rgb(113,147,255)"
+}
