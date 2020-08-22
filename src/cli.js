9e2b56dfffb99d9f7a99c15ded1a516a85e2d284
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-22 19:53:32 -0300

    feat(main): add --import and --input options

--- src/cli.js
+++ src/cli.js
@@ -15,10 +15,16 @@ async function main() {
     cli
         .helpOption("-h, --help", "Display command help")
         .option("-e, --env-file <path>", "Path of the .env file to load credentials from")
+        .option("-i, --input <path>", "Path of input file to load data from")
         .option("-o, --output <path>", "Path of output file for retrieved data")
+        .option("-m, --import", "Whether to only import data; not migrate from a new account")
         .option("-x, --export", "Whether to only export data; not migrate to a new account")
-
     cli.parse(process.argv)
+    if (cli.import && cli.export) {
+        console.log(chalk`{red Only one of {redBright --import} and {redBright --export} can be specified.}`)
+        process.exit(1)
+    }
+
     const credentials = await loadCredentials(cli)
 }
 
