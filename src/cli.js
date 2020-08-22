a9ff4cb273c1405cda6d0c9d25df31ba338601df
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-22 19:40:42 -0300

    fix(main): fix empty match error inside exception handler

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ const loadCredentials = require("./load-credentials")
 
 process.on("unhandledRejection", error => {
     console.log(chalk`{red Unexpected Error:} {redBright ${error.message}}`)
-    const stack = chalk.gray(error.stack.match(/(\n    at(.+(reddit-migrate.src).+))+/g).join("\n"))
+    const stack = chalk.gray((error.stack.match(/\n    at(.+(reddit-migrate.src).+)/g) || []).join("\n"))
     console.log(stack)
 })
 
