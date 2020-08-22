4d09506401a6b320667ead55e72750e1e82bd301
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-22 20:12:14 -0300

    feat(util): add error function

--- src/util.js
+++ src/util.js
@@ -6,5 +6,10 @@ module.exports = {
     bgOrange: chalk.bgRgb(255, 69, 0),
     bgOrangeString: "bgRgb(255,69,0)",
     blue: chalk.rgb(113, 147, 255),
-    blueString: "rgb(113,147,255)"
+    blueString: "rgb(113,147,255)",
+    error(message, exit = true) {
+        const highlighted = message.replace(/{(.+)}/, (_, $1) => chalk.redBright($1))
+        console.log(chalk.red(highlighted))
+        if (exit) process.exit(1)
+    }
 }
