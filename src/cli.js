c5246424c5e374cfa9a58f59a95f646e83bbdebf
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-23 20:55:51 -0300

    refactor(main): add 'data' to --input description

--- src/cli.js
+++ src/cli.js
@@ -12,6 +12,7 @@ process.on("unhandledRejection", ({ message, stack }) => {
     console.log(error(`Unexpected Error: {${message}}`))
     const prettyStack = chalk.gray((stack.match(/    at(.+(reddit-migrate.src).+)/g) || []).join("\n"))
     console.log(prettyStack)
+    process.exit(1)
 })
 
 async function main() {
@@ -22,7 +23,7 @@ async function main() {
         .option("-e, --env-file <path>", "Path of the .env file to load credentials from")
         .option("-m, --import", "Whether to only import data; not migrate from a new account")
         .option("-x, --export", "Whether to only export data; not migrate to a new account")
-        .option("-i, --input <path>", "Path of input file to import from")
+        .option("-i, --input <path>", "Path of input file to import data from")
         .option("-o, --output <path>", "Path of output file to export data to")
 
     cli.parse(process.argv)
