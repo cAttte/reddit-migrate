c5d6eb927f22b4d7734d3d6c1f5c26c05048d65c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-23 06:13:29 -0300

    feat(main): implement login() and exportData()

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,8 @@ const cli = require("commander").program
 
 cli.helpInformation = require("./help")
 const loadCredentials = require("./credentials/load")
+const login = require("./login")
+const exportData = require("./data/export")
 
 process.on("unhandledRejection", ({ message, stack }) => {
     console.log(error(`Unexpected Error: {${message}}`))
@@ -18,15 +20,31 @@ async function main() {
     cli
         .helpOption("-h, --help", "Display command help")
         .option("-e, --env-file <path>", "Path of the .env file to load credentials from")
-        .option("-i, --input <path>", "Path of input file to load data from")
-        .option("-o, --output <path>", "Path of output file for retrieved data")
         .option("-m, --import", "Whether to only import data; not migrate from a new account")
         .option("-x, --export", "Whether to only export data; not migrate to a new account")
+        .option("-i, --input <path>", "Path of input file to import from")
+        .option("-o, --output <path>", "Path of output file to export data to")
+
     cli.parse(process.argv)
     if (cli.import && cli.export)
         error("Only one of {--import} and {--export} can be specified.")
+    if (cli.import && !cli.input)
+        error("{--input} must be specified when {--import} is present.")
+    if (cli.export && !cli.output)
+        error("{--output} must be specified when {--export} is present.")
+    cli.migrate = !cli.import && !cli.export
 
-    const credentials = await loadCredentials(cli)
+    cli.credentials = await loadCredentials(cli)
+
+    if (cli.migrate) {
+        const old = await login(
+            cli.credentials.OLD_CLIENT_ID,
+            cli.credentials.OLD_CLIENT_SECRET,
+            cli.credentials.OLD_USERNAME,
+            cli.credentials.OLD_PASSWORD
+        )
+        const data = await exportData(cli, old)
+    }
 }
 
 main()
