a540d07d5074aa7fe3a41d200602703651276d55
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-22 21:15:47 -0300

    refactor(login): implement util.success()

--- src/login.js
+++ src/login.js
@@ -1,6 +1,5 @@
 const package = require("../package.json")
-const chalk = require("chalk")
-const { error, spin } = require("./util")
+const { success, error, spin } = require("./util")
 const Snoowrap = require("snoowrap")
 
 module.exports = async function login(id, secret, username, password) {
@@ -20,6 +19,6 @@ module.exports = async function login(id, secret, username, password) {
     })
 
     spinner.stop(true)
-    console.log(chalk`{green Successfully logged in as} {greenBright ${username}}{green .}`)
+    console.log(success(`Successfully logged in as {${username}}.`))
     return reddit
 }
