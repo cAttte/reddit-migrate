b92af360b6f16e007fae6c8d37c39a003c0bb20b
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-22 23:38:04 -0300

    refactor(util): move all properties to individual variables
    also use custom color for symbols.info

--- src/util.js
+++ src/util.js
@@ -1,47 +1,62 @@
-const symbols = require("log-symbols")
+const stripANSI = require("strip-ansi")
+const logSymbols = require("log-symbols")
 const chalk = require("chalk")
 const ora = require("ora")
 
-const highlight = (string, color) => {
+// colors
+const orange = chalk.rgb(255, 69, 0)
+const orangeString = "rgb(255,69,0)"
+const bgOrange = chalk.bgRgb(255, 69, 0)
+const bgOrangeString = "bgRgb(255,69,0)"
+const blue = chalk.rgb(113, 147, 255)
+const blueString = "rgb(113,147,255)"
+
+// symbols
+const symbols = {
+    info: util.blue(stripANSI(symbols.success)),
+    ...logSymbols
+},
+
+// logging
+function highlight(string, color) {
     const colorize = chalk[color + "Bright"]
     return string.replace(/{(.+?)}/g, (_, $1) => colorize($1))
 }
-
-const util = {
-    // colors
-    orange: chalk.rgb(255, 69, 0),
-    orangeString: "rgb(255,69,0)",
-    bgOrange: chalk.bgRgb(255, 69, 0),
-    bgOrangeString: "bgRgb(255,69,0)",
-    blue: chalk.rgb(113, 147, 255),
-    blueString: "rgb(113,147,255)",
-
-    // symbols
-    symbols: symbols,
-
-    // logging
-    formatSuccess(message) {
-        return highlight(message, "green")
-    },
-    success(message) {
-        console.log(`${util.symbols.success} ${util.formatSuccess(message)}`)
-    },
-    formatError(message) {
-        return highlight(message, "red")
-    },
-    error(message, exit) {
-        console.log(`${util.symbols.error} ${util.formatError(message)}`)
-        if (exit) process.exit(1)
-    },
-    spin(message, options = {}) {
-        const spinner = ora({
-            text: highlight(message, "yellow"),
-            spinner: "line",
-            color: "yellow",
-            ...options
-        })
-        return spinner.start()
-    }
+function formatSuccess(message) {
+    return highlight(message, "green")
+}
+function success(message) {
+    console.log(`${symbols.success} ${formatSuccess(message)}`)
+}
+function formatError(message) {
+    return highlight(message, "red")
+}
+function error(message, exit) {
+    console.log(`${symbols.error} ${formatError(message)}`)
+    if (exit) process.exit(1)
+}
+function spin(message, options = {}) {
+    const spinner = ora({
+        text: highlight(message, "yellow"),
+        spinner: "line",
+        color: "yellow",
+        ...options
+    })
+    return spinner.start()
 }
 
-module.exports = util
+module.exports = {
+    orange,
+    orangeString,
+    bgOrange,
+    bgOrangeString,
+    blue,
+    blueString,
+    symbols,
+    highlight,
+    formatSuccess,
+    success,
+    formatError,
+    error,
+    spin
+}
