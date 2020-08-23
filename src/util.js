42798099bccf635391b5dbe591bc1c6356c6d706
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-22 21:02:27 -0300

    feat(util): add spin function

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 const chalk = require("chalk")
+const { Spinner } = require("cli-spinner")
 
 module.exports = {
     orange: chalk.rgb(255, 69, 0),
@@ -11,5 +12,13 @@ module.exports = {
         const highlighted = message.replace(/{(.+?)}/g, (_, $1) => chalk.redBright($1))
         console.log(chalk.red(highlighted))
         if (exit) process.exit(1)
+    },
+    spin(message) {
+        const highlighted = message.replace(/{(.+?)}/g, (_, $1) => chalk.yellowBright($1))
+        const spinner = new Spinner()
+            .setSpinnerTitle(chalk.yellow("%s " + highlighted))
+            .setSpinnerString(0)
+            .start()
+        return spinner
     }
 }
