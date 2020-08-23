c8361a6c00481fef2cc36019f1bdf26379ed6061
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-23 06:17:27 -0300

    fix(exportPreferences): remove test line

--- src/data/export/preferences.js
+++ src/data/export/preferences.js
@@ -4,6 +4,5 @@ module.exports = async function exportPreferences(reddit) {
     const spinner = spin("Retrieving preferences...")
     const preferences = await reddit.getPreferences()
     spinner.succeed(formatSuccess(`Retrieved {${Object.keys(preferences).length}} preferences.`))
-    fs.writeFileSync("./pref.json", JSON.stringify(preferences, null, 4))
     return preferences
 }
