968de03fb87a47959103bd6114e2083d7692c18e
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-26 17:54:05 -0300

    feat(importSubscriptions): also handle following users

--- src/data/import/subscriptions.js
+++ src/data/import/subscriptions.js
@@ -1,9 +1,10 @@
 const { highlight, formatSuccess, formatError, spin, error } = require("../../util")
 
-module.exports = async function importSubscriptions(reddit, data) {
+async function subscribe(reddit, data, { action, actioning, actioned, object }) {
     if (!data || !Array.isArray(data) || !data.length)
-        return error("No subreddits to subscribe to.", false)
-    const spinner = spin("Subscribing to subreddits...")
+        error(`No ${object}s to ${action}.`, false)
+
+    const spinner = spin(`${actioning} ${object}s...`)
     let succeeded = 0
     let failed = 0
     for (i = 0; i < data.length; i++) {
@@ -12,12 +13,27 @@ module.exports = async function importSubscriptions(reddit, data) {
         await sub.subscribe()
             .then(() => succeeded++)
             .catch(() => failed++)
-        spinner.text = highlight(`Subscribing to {${succeeded}} subreddits...`, "yellow")
+        spinner.text = highlight(`${actioning} {${succeeded}} ${object}s...`, "yellow")
             + (failed ? highlight(` ({${failed}} failed)`, "red") : "")
     }
     if (!succeeded)
-        spinner.fail(formatError(`Couldn't subscribe to {${failed}} subreddits.`))
+        spinner.fail(formatError(`Couldn't ${action} {${failed}} ${object}s.`))
     else
-        spinner.succeed(formatSuccess(`Subscribed to {${succeeded}} subreddits.`)
-            + (failed ? formatError(` Couldn't subscribe to {${failed}}.`) : ""))
+        spinner.succeed(formatSuccess(`${actioned} {${succeeded}} ${object}s.`)
+            + (failed ? formatError(` Couldn't ${action} {${failed}}.`) : ""))
+}
+
+module.exports = async function importSubscriptions(reddit, subreddits, users) {
+    await subscribe(reddit, subreddits, {
+        action: "subscribe to",
+        actioning: "Subscribing to",
+        actioned: "Subscribed to",
+        object: "subreddit"
+    })
+    await subscribe(reddit, users.map(u => `u_${u}`), {
+        action: "follow",
+        actioning: "Following",
+        actioned: "Followed",
+        object: "user"
+    })
 }
