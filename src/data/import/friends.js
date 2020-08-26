74e8088d86206d66f5480094b7c056cbff8efb41
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-26 20:30:33 -0300

    feat(import): implement importFriends()

---
+++ src/data/import/friends.js
@@ -0,0 +1,23 @@
+const { highlight, formatSuccess, formatError, spin, error } = require("../../util")
+
+module.exports = async function importFriends(reddit, data) {
+    if (!data || !Array.isArray(data) || !data.length)
+        return error("No users to friend.", false)
+
+    const spinner = spin("Friending users...")
+    let succeeded = 0
+    let failed = 0
+    for (i = 0; i < data.length; i++) {
+        const user = await reddit.getUser(data[i])
+        await user.friend()
+            .then(() => succeeded++)
+            .catch(() => failed++)
+        spinner.text = highlight(`Friending {${succeeded}} users...`, "yellow")
+            + (failed ? highlight(` ({${failed}} failed)`, "red") : "")
+    }
+    if (!succeeded)
+        spinner.fail(formatError(`Couldn't friend {${failed}} users.`))
+    else
+        spinner.succeed(formatSuccess(`Friended {${succeeded}} users.`)
+            + (failed ? formatError(` Couldn't friend {${failed}}.`) : ""))
+}
