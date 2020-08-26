c97e8ad8b5bb4d05cfa0cce1f66420d38e3bfa96
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-26 17:35:41 -0300

    feat: implement importSubscriptions()

---
+++ src/data/import/subscriptions.js
@@ -0,0 +1,22 @@
+const { highlight, formatSuccess, formatError, spin, error } = require("../../util")
+
+module.exports = async function importSubscriptions(reddit, data) {
+    if (!data || !Array.isArray(data)) error("Subreddits unavailable.", false)
+    const spinner = spin("Subscribing to subreddits...")
+    let succeeded = 0
+    let failed = 0
+    for (i = 0; i < data.length; i++) {
+        const name = data[i]
+        const sub = await reddit.getSubreddit(name)
+        await sub.subscribe()
+            .then(() => succeeded++)
+            .catch(() => failed++)
+        spinner.text = highlight(`Subscribing to {${succeeded}} subreddits...`, "yellow")
+            + (failed ? highlight(` ({${failed}} failed)`, "red") : "")
+    }
+    if (!succeeded)
+        spinner.fail(formatError(`Couldn't subscribe to {${failed}} subreddits.`))
+    else
+        spinner.succeed(formatSuccess(`Subscribed to {${succeeded}} subreddits.`)
+            + (failed ? formatError(` Couldn't subscribe to {${failed}}.`) : ""))
+}
