e35624c1577bc8f947b995687b567d6e71bf7574
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-27 18:16:27 -0300

    feat: implement migrate command

--- src/commands/migrate.js
+++ src/commands/migrate.js
@@ -1,5 +1,12 @@
-const { success } = require("../util")
+const loadCredentials = require("../credentials/load")
+const login = require("../login")
+const exportData = require("../data/export")
+const importData = require("../data/import")
 
-module.exports = function migrateCommand() {
-    success("Migrate command")
+module.exports = async function exportCommand(self) {
+    const credentials = await loadCredentials(this, true)
+    const oldReddit = await login(credentials, "OLD_")
+    const data = await exportData(oldReddit)
+    const newReddit = await login(credentials, "NEW_")
+    await importData(newReddit, data)
 }
