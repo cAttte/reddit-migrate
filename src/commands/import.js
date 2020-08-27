babb42f3e60eca28823ef62881fc5071c8b0d2fe
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-27 17:36:58 -0300

    feat: implement import command

--- src/commands/import.js
+++ src/commands/import.js
@@ -1,5 +1,26 @@
-const { success } = require("../util")
+const fs = require("fs/promises")
+const path = require("path")
 
-module.exports = function importCommand() {
-    success("Import command")
+const { spin, formatSuccess, formatError } = require("../util")
+const loadCredentials = require("../credentials/load")
+const login = require("../login")
+const importData = require("../data/import")
+
+module.exports = async function importCommand(self) {
+    const credentials = await loadCredentials(this, false)
+    const reddit = await login(credentials)
+    const input = path.resolve(self.input)
+    const spinner = spin(`Reading data from {${input}}...`)
+    const data = await fs.readFile(input)
+        .then(input => JSON.parse(input))
+        .catch(e => {
+            spinner.fail(formatError(`Couldn't read {${input}}: ${e.message.replace(/, open.+/, "")}`))
+            process.exit(1)
+        })
+    spinner.succeed(formatSuccess(`Read data from {${input}}.`))
+
+    const exportDate = data.exported_at ? new Date(data.exported_at) : NaN
+    if (!isNaN(exportDate)) console.log(formatSuccess(`  Exported at {${exportDate}}.`))
+
+    await importData(reddit, data)
 }
