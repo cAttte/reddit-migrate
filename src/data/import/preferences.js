4c57544fdb617863136d1232ec8cef29a12e2657
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-27 00:28:50 -0300

    feat: implement importPreferences()

---
+++ src/data/import/preferences.js
@@ -0,0 +1,22 @@
+const { formatSuccess, spin } = require("../../util")
+
+module.exports = async function importPreferences(reddit, data) {
+    if (!data || typeof data !== "object" || !Object.entries(data).length)
+        return error("No preferences to update.", false)
+
+    const spinner = spin("Updating preferences...")
+    const oldPreferences = await reddit.getPreferences()
+
+    await reddit.updatePreferences(data)
+        .then(async () => {
+            const newPreferences = await reddit.getPreferences()
+            let updated = 0
+            for (pref of Object.keys(newPreferences))
+                if (oldPreferences[pref] !== newPreferences[pref])
+                    updated++
+            spinner.succeed(formatSuccess(`Updated {${updated}} preferences.`))
+        })
+        .catch(e => {
+            spinner.fail(`Couldn't update preferences: ${e.message}`)
+        })
+}
