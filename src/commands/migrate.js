c3111806c6d9035dd9d8e1861b6434aac143b109
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-28 06:25:35 -0300

    feat(migrate): implement --which option

--- src/commands/migrate.js
+++ src/commands/migrate.js
@@ -3,10 +3,10 @@ const login = require("../login")
 const exportData = require("../data/export")
 const importData = require("../data/import")
 
-module.exports = async function exportCommand(self) {
+module.exports = async function migrateCommand(self) {
     const credentials = await loadCredentials(this, true)
     const oldReddit = await login(credentials, "OLD_")
-    const data = await exportData(oldReddit)
+    const data = await exportData(oldReddit, self.which)
     const newReddit = await login(credentials, "NEW_")
-    await importData(newReddit, data)
+    await importData(newReddit, data, self.which)
 }
