41b10ab843c555565363a4278e0a6dc64b3ac0af
cAttte <26514199+cAttte@users.noreply.github.com>
2020-08-27 23:08:18 -0300

    fix(loadCredentials): avoid returning spinner from .catch()

--- src/credentials/load.js
+++ src/credentials/load.js
@@ -36,7 +36,7 @@ module.exports = async function loadCredentials(command, needBoth) {
     if (filepath) {
         const spinner = spin(`Loading {${filepath}}...`)
         const buffer = await fs.promises.readFile(path.resolve(filepath))
-            .catch(() => spinner.fail(formatError(`Couldn't read {${filepath}}.`)))
+            .catch(() => { spinner.fail(formatError(`Couldn't read {${filepath}}.`)) })
         if (buffer) {
             const parsed = dotenv.parse(buffer)
             Object.assign(env, parsed)
