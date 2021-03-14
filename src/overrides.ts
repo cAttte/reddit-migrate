81477d638c895d15b8f7bb78262045657239672e
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 14:12:42 -0300

    refactor(overrides): rewrite in TypeScript

--- src/overrides.ts
+++ src/overrides.ts
@@ -1,13 +1,15 @@
-const { error } = require("./util")
+import commander from "commander"
+import { error } from "./util"
 
-module.exports = function override(command) {
+/**
+ * Override some of Commander's default methods to stick to the CLI's logging style.
+ */
+export function override(command: commander.Command): void {
     command.missingArgument = () => error(`Missing required argument {${name}}.`)
     command.optionMissingArgument = ({ flags }) =>
         error(`Option {${flags}} argument missing.`)
     command.missingMandatoryOptionValue = ({ flags }) =>
         error(`Required option {${flags}} not specified.`)
-    command.unknownOption = flag => error(`Unknown option {${flag}}.`)
+    command.unknownOption = (flag: string) => error(`Unknown option {${flag}}.`)
     command.unknownCommand = () => error(`Unknown command {${command.args[0]}}.`)
-
-    return command
 }
