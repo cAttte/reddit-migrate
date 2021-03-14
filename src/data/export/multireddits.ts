3a574b7f14b5b7bdf0a5dd6af4bacb71e0c8992a
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 15:53:34 -0300

    refactor(export/multireddits): rewrite in TypeScript

--- src/data/export/multireddits.ts
+++ src/data/export/multireddits.ts
@@ -1,17 +1,21 @@
-const { spin, formatSuccess } = require("../../util")
+import Snoowrap from "snoowrap"
+import { spin, formatSuccess } from "../../util"
 
-module.exports = async function exportMultireddits(reddit) {
+export default async function exportMultireddits(reddit: Snoowrap) {
     const spinner = spin("Retrieving multireddits...")
     const multireddits = await reddit.getMyMultireddits()
     spinner.succeed(formatSuccess(`Retrieved {${multireddits.length}} multireddits.`))
+
     const data = multireddits.map(multi => ({
         name: multi.name,
         description: multi.description_md,
-        over_18: multi.over_18,
         visibility: multi.visibility,
+        icon_name: multi.icon_name,
         key_color: multi.key_color,
+        weighting_scheme: multi.weighting_schema,
         copied_from: multi.copied_from,
         subreddits: multi.subreddits.map(sub => sub.display_name)
     }))
+
     return data
 }
