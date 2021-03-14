e86e759a6ff800bd8abe9b3fbfd0a70eb400cd5d
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 15:29:00 -0300

    refactor(data/export): rewrite in TypeScript

--- src/data/export.ts
+++ src/data/export.ts
@@ -1,23 +1,28 @@
-const exportSubscriptions = require("./export/subscriptions")
-const exportFriends = require("./export/friends")
-const exportBlocked = require("./export/blocked")
-const exportMultireddits = require("./export/multireddits")
-const exportProfile = require("./export/profile")
-const exportPreferences = require("./export/preferences")
+import Snoowrap from "snoowrap"
+import exportSubscriptions from "./export/subscriptions"
+import exportFriends from "./export/friends"
+import exportBlocked from "./export/blocked"
+import exportMultireddits from "./export/multireddits"
+import exportProfile from "./export/profile"
+import exportPreferences from "./export/preferences"
+import Data from "./Data"
+import Which from "./Which"
 
-module.exports = async function exportData(reddit, which) {
-    const data = { exported_at: null }
+module.exports = async function exportData(reddit: Snoowrap, which: Which) {
+    const data: Partial<Data> = { exported_at: null }
     if (which.subscriptions || which.follows) {
         const { subscriptions, follows } = await exportSubscriptions(reddit, which)
         if (which.subscriptions) data.subscriptions = subscriptions
         if (which.follows) data.follows = follows
     }
+
     if (which.friends) data.friends = await exportFriends(reddit)
     if (which.blocked) data.blocked = await exportBlocked(reddit)
     if (which.multireddits) data.multireddits = await exportMultireddits(reddit)
     if (which.profile) data.profile = await exportProfile(reddit)
     if (which.preferences) data.preferences = await exportPreferences(reddit)
     data.exported_at = new Date().toISOString()
+
     console.log()
     return data
 }
