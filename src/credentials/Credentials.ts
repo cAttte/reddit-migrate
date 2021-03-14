1555e0dcf6467c4dd1b1975c89426820ba6b9eda
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 14:17:53 -0300

    feat: create Credentials interface

---
+++ src/credentials/Credentials.ts
@@ -0,0 +1,14 @@
+export interface Credentials {
+    CLIENT_ID: string
+    CLIENT_SECRET: string
+    USERNAME: string
+    PASSWORD: string
+    OLD_CLIENT_ID?: string
+    OLD_CLIENT_SECRET?: string
+    OLD_USERNAME?: string
+    OLD_PASSWORD?: string
+    NEW_CLIENT_ID?: string
+    NEW_CLIENT_SECRET?: string
+    NEW_USERNAME?: string
+    NEW_PASSWORD?: string
+}
