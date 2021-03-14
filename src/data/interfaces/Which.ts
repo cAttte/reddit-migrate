0032367c67d85fbba9e8d1d50fcf5dfc48b41d0f
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 17:19:35 -0300

    docs(Which): add comment

--- src/data/interfaces/Which.ts
+++ src/data/interfaces/Which.ts
@@ -1,5 +1,8 @@
 import { Attributes, Submissions } from "./Attributes"
 
+/**
+ * <S> should be true if in a submission context. so, if the command being used is `purge`.
+ */
 type Which<S extends boolean = false> = Partial<
     Record<S extends true ? keyof typeof Submissions : keyof typeof Attributes, true>
 >
