c33e48074e468679f03175ec3ceea3770773dcc7
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 16:52:27 -0300

    refactor(export/profile): type-cast to object

--- src/data/export/profile.ts
+++ src/data/export/profile.ts
@@ -1,10 +1,7 @@
 import Snoowrap from "snoowrap"
 import { spin, formatSuccess } from "../../util"
 
-// @ts-ignore: Snoowrap typings are broken
-export default async function exportProfile(
-    reddit: Snoowrap
-): Promise<Snoowrap.Subreddit> {
+export default async function exportProfile(reddit: Snoowrap) {
     const spinner = spin("Retrieving profile settings...")
     // JSON to remove all proxy-related shit which causes a lot of errors
     // @ts-ignore: Snoowrap typings are broken
@@ -13,5 +10,5 @@ export default async function exportProfile(
         formatSuccess(`Retrieved {${Object.keys(me).length}} profile settings.`)
     )
 
-    return me.subreddit
+    return me.subreddit as Record<string, any>
 }
