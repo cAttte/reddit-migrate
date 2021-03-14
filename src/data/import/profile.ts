8fc1da3bb91ae624a74bfd97f3bfccc9595e9322
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 16:03:45 -0300

    refactor(import/profile): rewrite in TypeScript (actually probably fix #3)

--- src/data/import/profile.ts
+++ src/data/import/profile.ts
@@ -1,23 +1,27 @@
-const { formatSuccess, spin } = require("../../util")
+import Snoowrap from "snoowrap"
+import { formatSuccess, spin, error } from "../../util"
+import Data from "../interfaces/Data"
 
-module.exports = async function importProfile(reddit, data) {
+export default async function importProfile(reddit: Snoowrap, data: Data["profile"]) {
     if (!data || typeof data !== "object" || !Object.entries(data).length)
         return error("No profile settings to update.", false)
 
     const spinner = spin("Updating profile settings...")
-    const userSubreddit = await reddit.getSubreddit(`u_${reddit.username}`).fetch()
+    // @ts-ignore: Snoowrap typings are broken
+    const userSubreddit: Snoowrap.Subreddit = await reddit
+        .getSubreddit(`u_${reddit.username}`)
+        .fetch()
     const oldSettings = await userSubreddit.getSettings()
 
     await userSubreddit
+        // @ts-ignore: missing properties or whatever
         .editSettings(data)
         .then(async () => {
             const newSettings = await userSubreddit.getSettings()
             let updated = 0
-            for (setting of Object.keys(newSettings))
+            for (const setting of Object.keys(newSettings))
                 if (oldSettings[setting] !== newSettings[setting]) updated++
             spinner.succeed(formatSuccess(`Updated {${updated}} profile settings.`))
         })
-        .catch(e => {
-            spinner.fail(`Couldn't update profile settings: ${e.message}`)
-        })
+        .catch(e => spinner.fail(`Couldn't update profile settings: ${e.message}`))
 }
