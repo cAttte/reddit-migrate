a48e39b6e3a64f10b60c41c7cf0dcae158b135ef
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 16:00:13 -0300

    refactor(export/preferences): rewrite in TypeScript

--- src/data/export/preferences.ts
+++ src/data/export/preferences.ts
@@ -1,10 +1,12 @@
-const { spin, formatSuccess } = require("../../util")
+import Snoowrap from "snoowrap"
+import { spin, formatSuccess } from "../../util"
 
-module.exports = async function exportPreferences(reddit) {
+export default async function exportPreferences(reddit: Snoowrap) {
     const spinner = spin("Retrieving preferences...")
     const preferences = await reddit.getPreferences()
     spinner.succeed(
         formatSuccess(`Retrieved {${Object.keys(preferences).length}} preferences.`)
     )
+
     return preferences
 }
