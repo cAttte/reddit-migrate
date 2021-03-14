b114d837140f26c1045f9462e9cb0794b4ba59df
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 16:59:16 -0300

    fix(*): delete old .js files

--- src/commands/purge.js
+++
@@ -1,9 +0,0 @@
-const loadCredentials = require("../credentials/load")
-const login = require("../login")
-const purge = require("../data/purge")
-
-module.exports = async function purgeCommand(self) {
-    const credentials = await loadCredentials(self, false)
-    const reddit = await login(credentials)
-    await purge(reddit, self.edit, self.which)
-}
--- src/data/export/friends.js
+++
@@ -1,8 +0,0 @@
-const { spin, formatSuccess } = require("../../util")
-
-module.exports = async function exportFriends(reddit) {
-    const spinner = spin("Retrieving friends...")
-    const friends = await reddit.getFriends().flat()
-    spinner.succeed(formatSuccess(`Retrieved {${friends.length}} friends.`))
-    return friends.map(f => f.name)
-}
