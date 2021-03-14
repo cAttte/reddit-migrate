3b7b26d54a877a61b03daf4b36daddb331c3be81
cAttte <26514199+cAttte@users.noreply.github.com>
2021-03-14 14:38:25 -0300

    fix(overrides): move fake helpOption here, add missing 'name' param to missingArgument

--- src/overrides.ts
+++ src/overrides.ts
@@ -5,11 +5,15 @@ import { error } from "./util"
  * Override some of Commander's default methods to stick to the CLI's logging style.
  */
 export default function override(command: commander.Command): void {
-    command.missingArgument = () => error(`Missing required argument {${name}}.`)
+    command.missingArgument = (name: string) =>
+        error(`Missing required argument {${name}}.`)
     command.optionMissingArgument = ({ flags }) =>
         error(`Option {${flags}} argument missing.`)
     command.missingMandatoryOptionValue = ({ flags }) =>
         error(`Required option {${flags}} not specified.`)
     command.unknownOption = (flag: string) => error(`Unknown option {${flag}}.`)
     command.unknownCommand = () => error(`Unknown command {${command.args[0]}}.`)
+
+    // this is the only way i thought of to "remove" the help option...
+    command.helpOption(`--${Math.random()}`, "")
 }
